1dc560fde43b085cd774283925d0bc819b73a884
mohankoli <mohankoli1990@gmail.com>
2023-02-25 16:14:51 +0530

    first commit;

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+import data from './data';
+import List from './List';
+
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthday Today</h3>
+        <List people={people}></List>
+        <button onClick={()=>{setPeople([])}}>clearAll</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
